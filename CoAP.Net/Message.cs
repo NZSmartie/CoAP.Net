8b46dba1a4fb6df08e19db178bb4ec8f8c27b60c
NZSmartie <nzsmartie@gmail.com>
2017-02-14 02:00:36 +1300

    Parse URIs to Options, and add examples from RFC7252 Appendix B.

--- CoAP.Net/Message.cs
+++ CoAP.Net/Message.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace CoAP.Net
 {
@@ -58,21 +58,64 @@ namespace CoAP.Net
         ProxyingNotSupported = 505
     }
 
-    class Message
+    public class Message
     {
         private byte[] _header;
 
-        public int Version { get; set;  }
+        public int Version { get; set; }
         public MessageType Type { get; set; }
         public byte[] Token { get; set; }
         public int Id { get; set; }
 
-        public List<Option> Options { get; set; }
+        private List<Option> _options = new List<Option>();
 
-        public Message()
+        public List<Option> Options
         {
-            
+            get { return _options; }
+            set { _options = value; }
         }
 
+        public Message() { }
+
+        public void FromUri(string input)
+        {
+            // Will throw exceptions that the application code can handle
+            Uri uri = new Uri(input);
+
+            if (!uri.IsAbsoluteUri)
+                throw new UriFormatException("URI is not absolute and unsupported by the CoAP scheme");
+
+            if (uri.Scheme != "coap" && uri.Scheme != "coaps")
+                throw new UriFormatException("Input URI scheme is not coap:// or coaps://");
+
+            if (uri.Fragment.Length > 0)
+                throw new UriFormatException("Fragments are unsupported in the CoAP scheme");
+
+            // Strip out any existing URI Options 
+            var optionsToDiscard = new int[] { RegisteredOptionNumber.UriHost, RegisteredOptionNumber.UriPort, RegisteredOptionNumber.UriPath, RegisteredOptionNumber.UriQuery };
+            _options = _options.Where(kv => !optionsToDiscard.Contains(kv.OptionNumber)).ToList();
+
+            switch (uri.HostNameType)
+            {
+                case UriHostNameType.Dns:
+                    _options.Add(new Options.UriHost { ValueString = uri.IdnHost });
+                    break;
+                case UriHostNameType.IPv4:
+                case UriHostNameType.IPv6:
+                    //_options.Add(new Options.UriHost { ValueString = uri.Host });
+                    break;
+                default:
+                    throw new UriFormatException("Unknown Hostname");
+            }
+
+            if ((uri.Scheme == "coap" && !uri.IsDefaultPort && uri.Port != 5683) ||
+                (uri.Scheme == "coaps" && !uri.IsDefaultPort && uri.Port != 5684))
+                _options.Add(new Options.UriPort { ValueUInt = (ushort)uri.Port });
+
+            _options.AddRange(uri.AbsolutePath.Substring(1).Split(new[] { '/' }).Select(p => new Options.UriPath { ValueString = Uri.UnescapeDataString(p) }));
+
+            if (uri.Query.Length > 0)
+                _options.AddRange(uri.Query.Substring(1).Split(new[] { '&' }).Select(p => new Options.UriQuery { ValueString = Uri.UnescapeDataString(p) }));
+        }
     }
 }
