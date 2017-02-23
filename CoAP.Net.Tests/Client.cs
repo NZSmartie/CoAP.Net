ef427a3416afe4070dcbcfc3bc83ecbf29ccfe77
NZSmartie <nzsmartie@gmail.com>
2017-02-24 07:16:07 +1300

    Improve flow of Send and Receive in CoapClient

--- CoAP.Net.Tests/Client.cs
+++ CoAP.Net.Tests/Client.cs
@@ -9,16 +9,23 @@ namespace CoAP.Net.Tests
     [TestClass]
     public class Client
     {
+
+        /// <summary>
+        /// Timout for any Tasks
+        /// </summary>
+        public const int MaxTaskTimeout = 2000;
+
         [TestMethod]
         public void TestClientRequest()
         {
+            // Arrange
             var mockClientEndpoint = new Mock<ICoapEndpoint>();
 
             mockClientEndpoint
                 .Setup(c => c.SendAsync(It.IsAny<CoapPayload>()))
-                .Returns(Task.CompletedTask)
-                .Verifiable();
+                .Returns(Task.CompletedTask);
 
+            // Act
             using (var client = new CoapClient(mockClientEndpoint.Object))
             {
                 client.SendAsync(new CoapMessage
@@ -26,14 +33,16 @@ namespace CoAP.Net.Tests
                     Type = CoapMessageType.Confirmable,
                     Code = CoapMessageCode.None
                 }).Wait();
-
-                Mock.Verify(mockClientEndpoint);
             }
-        }
 
+            // Assert
+            mockClientEndpoint.Verify(cep => cep.SendAsync(It.IsAny<CoapPayload>()));
+        }
+        
         [TestMethod]
         public void TestClientResponse()
         {
+            // Arrange
             var mockClientEndpoint = new Mock<ICoapEndpoint>();
             var mockPayload = new Mock<CoapPayload>();
 
@@ -57,24 +66,75 @@ namespace CoAP.Net.Tests
                 .Callback<CoapPayload>((p) => expected.Id = p.MessageId)
                 .Returns(Task.CompletedTask);
             mockClientEndpoint
-                .Setup(c => c.ReceiveAsync())
+                .SetupSequence(c => c.ReceiveAsync())
                 .Returns(Task.FromResult(mockPayload.Object))
-                .Verifiable();
+                .Throws(new CoapEndpointException());
 
+            // Ack
             using (var client = new CoapClient(mockClientEndpoint.Object))
             {
-                var responseTask = client.GetAsync("coap://example.com/.well-known/core");
-                responseTask.Wait();
+                // Sned message
+                var sendTask = client.GetAsync("coap://example.com/.well-known/core");
+                sendTask.Wait(MaxTaskTimeout);
 
-                // Ensure ICoapTransport.ReceiveAsync was called
-                Mock.Verify(mockClientEndpoint);
+                if (!sendTask.IsCompleted)
+                    throw new AssertFailedException("sendTask took too long to complete");
 
-                // Verify the message we got back is the expected response
-                Assert.AreEqual(expected.Id, responseTask.Result.Id);
-                Assert.AreEqual(CoapMessageType.Acknowledgement, responseTask.Result.Type);
-                Assert.AreEqual(CoapMessageCode.Content, responseTask.Result.Code);
+                client.Listen(); // enable loop back thingy
+
+                // Receive msssage
+                var responseTask = client.GetResponseAsync(sendTask.Result);
+                responseTask.Wait(MaxTaskTimeout);
+
+                if (!responseTask.IsCompleted)
+                    throw new AssertFailedException("responseTask took too long to complete");
 
             }
+
+            // Assert
+            mockClientEndpoint.Verify(x => x.ReceiveAsync(), Times.AtLeastOnce);
+        }
+
+        [TestMethod]
+        public void TestClientOnMessageReceivedEvent()
+        {
+            // Arrange
+            var expected = new CoapMessage
+            {
+                Id = 0x1234,
+                Type = CoapMessageType.Acknowledgement,
+                Code = CoapMessageCode.None,
+            };
+            var mockPayload = new Mock<CoapPayload>();
+            var mockClientEndpoint = new Mock<ICoapEndpoint>();
+            var clientOnMessageReceivedEventCalled = false;
+
+            mockPayload
+                .Setup(p => p.Payload)
+                .Returns(expected.Serialise());
+            mockClientEndpoint
+                .SetupSequence(c => c.ReceiveAsync())
+                .Returns(Task.FromResult(mockPayload.Object))
+                .Throws(new CoapEndpointException());
+
+            // Act
+            using (var mockClient = new CoapClient(mockClientEndpoint.Object))
+            {
+                var task = new TaskCompletionSource<bool>();
+                mockClient.OnMessageReceived += (s, e) =>
+                {
+                    clientOnMessageReceivedEventCalled = true;
+                    task.SetResult(true);
+                };
+
+                mockClient.Listen();
+
+                task.Task.Wait(MaxTaskTimeout);
+                mockClient.Dispose();
+            }
+
+            // Assert
+            Assert.IsTrue(clientOnMessageReceivedEventCalled);
         }
     }
 }
--- CoAP.Net/Client.cs
+++ CoAP.Net/Client.cs
@@ -5,42 +5,72 @@ using System.Threading.Tasks;
 
 namespace CoAP.Net
 {
+    public class CoapMessageReceivedEventArgs : EventArgs
+    {
+        public CoapMessage Message { get; set; }
+        public ICoapEndpoint Endpoint { get; set; }
+    }
+
+    public class CoapEndpointException : Exception { }
+
     public class CoapClient : IDisposable
     {
         private ICoapEndpoint _transport;
         private ushort _messageId;
 
         // I'm not particularly fond of the following _messageQueue and _messageResponses... Feels more like a hack. but it works? NEEDS MORE TESTING!!!
-        private ConcurrentDictionary<int, CoapMessage> _messageQueue = new ConcurrentDictionary<int, CoapMessage>();
-        private ConcurrentDictionary<int, TaskCompletionSource<CoapMessage>> _messageReponses = new ConcurrentDictionary<int, TaskCompletionSource<CoapMessage>>();
+        private ConcurrentDictionary<int, TaskCompletionSource<CoapMessage>> _messageReponses 
+            = new ConcurrentDictionary<int, TaskCompletionSource<CoapMessage>>();
 
         private CancellationTokenSource _receiveCancellationToken;
+
+        public event EventHandler<CoapMessageReceivedEventArgs> OnMessageReceived;
 
         public CoapClient(ICoapEndpoint transport)
         {
             _transport = transport;
 
             _messageId = (ushort)(new Random().Next() & 0xFFFFu);
+        }
+
+        public bool IsListening { get => _receiveCancellationToken != null && !_receiveCancellationToken.IsCancellationRequested; }
+
+        public void Listen() {
+            if (IsListening)
+                return;
 
             _receiveCancellationToken = new CancellationTokenSource();
 
             Task.Factory.StartNew(() =>
             {
                 var token = _receiveCancellationToken.Token;
+
                 while (!token.IsCancellationRequested)
                 {
-                    var payload = _transport.ReceiveAsync();
-                    payload.Wait(token);
-                    if (!payload.IsCompleted || payload.Result == null)
-                        continue;
+                    try
+                    {
+                        var payload = _transport.ReceiveAsync();
+                        payload.Wait(token);
+                        if (!payload.IsCompleted || payload.Result == null)
+                            continue;
 
-                    var message = new CoapMessage();
+                        var message = new CoapMessage();
 
-                    message.Deserialise(payload.Result.Payload);
+                        message.Deserialise(payload.Result.Payload);
 
-                    _messageQueue.TryAdd(message.Id, message);
-                    if (_messageReponses.ContainsKey(message.Id))
-                        _messageReponses[message.Id].TrySetResult(message);
+                        if (_messageReponses.ContainsKey(message.Id))
+                            _messageReponses[message.Id].TrySetResult(message);
+                        else
+                            OnMessageReceived?.Invoke(this, new CoapMessageReceivedEventArgs
+                            {
+                                Message = message,
+                                Endpoint = payload.Result.Endpoint
+                            });
+                    }
+                    catch(CoapEndpointException ex)
+                    {
+                        _receiveCancellationToken.Cancel();
+                    }
                 }
             });
         }
@@ -48,35 +78,32 @@ namespace CoAP.Net
         public void Dispose()
         {
             // Cancels our receiver task
-            _receiveCancellationToken.Cancel();
+            _receiveCancellationToken?.Cancel();
         }
 
-        protected Task<CoapMessage> GetResponseAsync(int messageId)
+        public async Task<CoapMessage> GetResponseAsync(int messageId)
         {
             TaskCompletionSource<CoapMessage> responseTask = null;
             if (!_messageReponses.TryGetValue(messageId, out responseTask))
                 throw new ArgumentOutOfRangeException("messageId does not exist");
 
-            // ToDo: Wait minimum timout period
-            responseTask.Task.Wait();
+            await responseTask.Task;
 
             // ToDo: if wait timed out, retry sending message with back-off delay
             _messageReponses.TryRemove(messageId, out responseTask);
-            CoapMessage _;
-            _messageQueue.TryRemove(messageId, out _);
 
-            return responseTask.Task;
+            return responseTask.Task.Result;
         }
 
-        public async Task SendAsync(CoapMessage message)
+        public async Task SendAsync(CoapMessage message, ICoapEndpoint endpoint = null)
         {
             if (message.Id == 0)
                 message.Id = _messageId++;
 
-            await _transport.SendAsync(new CoapPayload { Payload = message.Serialise(), MessageId = message.Id });
+            await _transport.SendAsync(new CoapPayload { Payload = message.Serialise(), MessageId = message.Id, Endpoint = endpoint });
         }
 
-        public async Task<CoapMessage> GetAsync(string uri)
+        public async Task<int> GetAsync(string uri, ICoapEndpoint endpoint = null)
         {
             var message = new CoapMessage
             {
@@ -88,9 +115,9 @@ namespace CoAP.Net
 
             _messageReponses.TryAdd(message.Id, new TaskCompletionSource<CoapMessage>());
 
-            await _transport.SendAsync(new CoapPayload { Payload = message.Serialise(), MessageId = message.Id });
+            await _transport.SendAsync(new CoapPayload { Payload = message.Serialise(), MessageId = message.Id, Endpoint = endpoint });
 
-            return await GetResponseAsync(message.Id);
+            return message.Id;
         }
     }
 }
