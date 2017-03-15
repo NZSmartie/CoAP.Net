f310b164de2ce9fc225cef6181eea970188b89ae
Roman Vaughan <nzsmartie@gmail.com>
2017-03-15 20:35:52 +1300

    Add CoRE LinkFormat parser

---
+++ CoAP.Net.Tests/CoreLinkFormat.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CoAP.Net;
+
+namespace CoAP.Net.Tests
+{
+    [TestClass]
+    public class TestCoreLinkFormat
+    {
+        [TestMethod]
+        [TestCategory("[RFC6690] Section 2")]
+        public void ParseSimpleLinkFormat()
+        {
+            // Arrange
+            var expected = new List<CoapResource>
+            {
+                new CoapResource("/sensor/temp")
+                {
+                    InterfaceDescription = new List<string>{ "sensor" }
+                },
+                new CoapResource("/sensor/light")
+                {
+                    InterfaceDescription = new List<string>{ "sensor" }
+                }
+            };
+
+            var message = "</sensor/temp>;if=\"sensor\",</sensor/light>;if=\"sensor\"";
+
+            // Act
+            var actual = CoreLinkFormat.Parse(message);
+
+            // Assert
+            Assert.IsTrue(expected.SequenceEqual(actual));
+        }
+
+        [TestMethod]
+        [TestCategory("[RFC6690] Section 2")]
+        public void ParseExtensiveLinkFormat()
+        {
+            // Arrange
+            var expected = new List<CoapResource>
+            {
+                new CoapResource("/sensor/temp")
+                {
+                    InterfaceDescription = new List<string>{"sensor", "read"},
+                    ResourceTypes = new List<string> { "temperature-c", "temperature-f" },
+                    Rev = new List<string>{"one", "two" },
+                    Rel = new List<string>{"one", "two" },
+                    HrefLang = "en-nz",
+                    Media = "none",
+                    Title = "Outside Temperature",
+                    TitleExt = "Primo Sensor"
+                },
+                new CoapResource("http://stupid.schema.io/temperature.json")
+                {
+                    Anchor = "/sensor/temp"
+                },
+                new CoapResource("/firmware/v2.1")
+                {
+                    ResourceTypes = new List<string>{ "firmware" },
+                    MaxSize = 262144
+                }
+            };
+
+            var message = "</sensor/temp>;if=\"sensor read\";rt=\"temperature-c temperature-f\";rev=\"one two\";rel=\"one two\";hreflang=en-nz;media=none;title=\"Outside Temperature\";title*=utf-8'en-nz'\"Primo Sensor\""
+                + ",<http://stupid.schema.io/temperature.json>;anchor=\"/sensor/temp\""
+                + ",</firmware/v2.1>;rt=\"firmware\";sz=262144";
+
+            // Act
+            var actual = CoreLinkFormat.Parse(message);
+
+            // Assert
+            Assert.IsTrue(expected.SequenceEqual(actual));
+        }
+    }
+}
---
+++ CoAP.Net/CoreLinkFormat.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoAP.Net
+{
+    public class CoapResource
+    {
+        public readonly string URIReference;
+
+        public List<string> Rel;
+
+        public string Anchor;
+
+        public List<string> Rev;
+
+        public string HrefLang;
+
+        public string Media;
+
+        public string Title;
+
+        public string TitleExt;
+
+        public string Type;
+
+        public List<string> ResourceTypes;
+
+        public List<string> InterfaceDescription;
+
+        public ulong MaxSize;
+
+        public Dictionary<string, string> Extentions = new Dictionary<string, string>();
+
+        public CoapResource(string uri)
+        {
+            URIReference = uri;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as CoapResource;
+
+            if (other == null)
+                return base.Equals(obj);
+
+            Func<ICollection<string>, ICollection<string>, bool> nullableSequenceEquals = 
+                (a, b) => (a == null && b == null) || (a != null && b != null && a.SequenceEqual(b));
+
+            if (URIReference != other.URIReference)
+                return false;
+            if (Anchor != other.Anchor)
+                return false;
+            if (HrefLang != other.HrefLang)
+                return false;
+            if (Media != other.Media)
+                return false;
+            if (Title != other.Title)
+                return false;
+            if (TitleExt != other.TitleExt)
+                return false;
+            if (Type != other.Type)
+                return false;
+            if (!nullableSequenceEquals(Rel, other.Rel))
+                return false;
+            if (!nullableSequenceEquals(Rev, other.Rev))
+                return false;
+            if (!nullableSequenceEquals(ResourceTypes, other.ResourceTypes))
+                return false;
+            if (!nullableSequenceEquals(InterfaceDescription, other.InterfaceDescription))
+                return false;
+            if (MaxSize != other.MaxSize)
+                return false;
+            if (!Extentions.SequenceEqual(other.Extentions))
+                return false;
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return URIReference.GetHashCode();
+        }
+    }
+
+    public static class CoreLinkFormat
+    {
+        private enum _formatState { LinkValue, LinkParam }
+
+        public static List<CoapResource> Parse(string message)
+        {
+            var state = _formatState.LinkValue;
+            int mPos = 0, mSeek;
+            string param, value;
+
+            var result = new List<CoapResource>();
+            CoapResource currentResource = null;
+            try
+            {
+                var run = true;
+                do
+                {
+                    switch (state)
+                    {
+                        case _formatState.LinkValue:
+                            if (message[mPos++] != '<')
+                                throw new ArgumentException(string.Format("Expected link-value '<' at pos {0}", mPos));
+
+                            mSeek = message.IndexOf('>', mPos);
+                            if (currentResource != null)
+                                result.Add(currentResource);
+                            currentResource = new CoapResource(message.Substring(mPos, mSeek - mPos));
+                            mPos = mSeek + 1;
+                            break;
+                        case _formatState.LinkParam:
+                            mSeek = message.IndexOf('=', mPos);
+                            param = message.Substring(mPos, mSeek - mPos);
+
+                            mPos = mSeek + 1;
+                            mSeek = message.IndexOfAny(new char[] { ',', ';' }, mPos);
+                            if (mSeek == -1)
+                                mSeek = message.Length;
+                            value = message.Substring(mPos, mSeek - mPos);
+
+                            if(param == "if")
+                            {
+                                value = value.Substring(1, value.Length - 2);
+                                currentResource.InterfaceDescription = value.Split(new char[] { ' ' }).ToList();
+                            }else if (param == "rt")
+                            {
+                                value = value.Substring(1, value.Length - 2);
+                                currentResource.ResourceTypes = value.Split(new char[] { ' ' }).ToList();
+                            } else if (param == "rev")
+                            {
+                                if (currentResource.Rev == null)
+                                {
+                                    value = value.Substring(1, value.Length - 2);
+                                    currentResource.Rev = value.Split(new char[] { ' ' }).ToList();
+                                }
+                            }
+                            else if (param == "rel")
+                            {
+                                if (currentResource.Rel == null)
+                                {
+                                    value = value.Substring(1, value.Length - 2);
+                                    currentResource.Rel = value.Split(new char[] { ' ' }).ToList();
+                                }
+                            }
+                            else if (param == "anchor")
+                            {
+                                currentResource.Anchor = value.Substring(1, value.Length - 2);
+                            }
+                            else if (param == "hreflang")
+                            {
+                                // Much easier to let libraries offload language formatting stuff
+                                currentResource.HrefLang = new System.Globalization.CultureInfo(value).Name.ToLower();
+                            }
+                            else if (param == "media")
+                            {
+                                if(value[0] == '"')
+                                {
+                                    if (value[value.Length - 1] != '"')
+                                        throw new ArgumentException(string.Format("Expected MediaDesc DQUOTE '\"' at pos {0}", mSeek));
+                                    value = value.Substring(1, value.Length - 2);
+                                }
+                                currentResource.Media = value;
+                            }
+                            else if (param == "title")
+                            {
+                                if (value[0] != '"' )
+                                    throw new ArgumentException(string.Format("Expected QuotedString DQUOTE '\"' at pos {0}", mPos));
+                                if (value[value.Length - 1] != '"')
+                                    throw new ArgumentException(string.Format("Expected QuotedString DQUOTE '\"' at pos {0}", mSeek));
+                                currentResource.Title = value.Substring(1, value.Length - 2);
+                            }
+                            else if (param == "title*")
+                            {
+                                // Todo: No idea what to do here...?
+                                var charset = value.Substring(0, value.IndexOf('\''));
+                                var lang = value.Substring(charset.Length + 1, value.IndexOf('\'', charset.Length + 1) - charset.Length - 1);
+                                value = value.Substring(charset.Length + lang.Length + 3, value.Length - charset.Length - lang.Length - 4);
+
+                                //System.Diagnostics.Debug.WriteLine("title* = {3}\n\tCharset: {0}\n\tLanguage: {1}\n\tValue: {2}", 
+                                //    charset, lang, value, Uri.UnescapeDataString(value));
+
+                                currentResource.TitleExt = Uri.UnescapeDataString(value);
+                            }
+                            else if (param == "type")
+                            {
+                                if (value[0] == '"')
+                                {
+                                    if (value[value.Length - 1] != '"')
+                                        throw new ArgumentException(string.Format("Expected Type DQUOTE '\"' at pos {0}", mSeek));
+                                    value = value.Substring(1, value.Length - 2);
+                                }
+                                currentResource.Type = value;
+                            }
+                            else if (param == "sz")
+                            {
+                                if (value[0] == '0' && value.Length != 1)
+                                    throw new ArgumentException(string.Format("cardinal may not start with '0' unless at pos {0}", mSeek));
+                                if(!ulong.TryParse(value, out currentResource.MaxSize))
+                                    throw new ArgumentException(string.Format("Could not parse cardinal at pos {0}", mSeek));
+                            }
+                            else
+                            {
+                                if (value.Length == 1)
+                                {
+                                    if (!new char[] { '!', '#', '$', '%', '&', '\'', '(', ')', '*', '+', '-', '.', '/', ':', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~' }.Contains(value[0]))
+                                        throw new ArgumentException(string.Format("PToken contains invalid character '{0}' at pos {1}", value[0], mSeek));
+                                    currentResource.Extentions.Add(param, value);
+                                }
+                                else
+                                {
+                                    if (value[0] != '"')
+                                        throw new ArgumentException(string.Format("Expected QuotedString DQUOTE '\"' at pos {0}", mPos));
+                                    if (value[value.Length - 1] != '"')
+                                        throw new ArgumentException(string.Format("Expected QuotedString DQUOTE '\"' at pos {0}", mSeek));
+                                    currentResource.Extentions.Add(param, value.Substring(1, value.Length - 2));
+                                }
+                            }
+
+                            mPos = mSeek;
+                            break;
+                        default:
+                            run = false;
+                            break;
+                    }
+                    if (message[mPos] == ';')
+                        state = _formatState.LinkParam;
+                    else if (message[mPos] == ',')
+                        state = _formatState.LinkValue;
+                    else
+                        throw new ArgumentException(string.Format("Invalid character '{0}' at pos {1}", message[mPos], mPos));
+                    mPos++;
+                }
+                while (run);
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+
+            }
+
+            if (currentResource != null)
+                result.Add(currentResource);
+
+            return result;
+        }
+    }
+}
