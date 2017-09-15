6f00c29f8ede95cfdd157f5069267a5af26f880f
NZSmartie <nzsmartie@gmail.com>
2017-09-16 01:23:44 +1200

    Use docfx for building static HTML documentation

---
+++ docs/build.ps1
@@ -0,0 +1,21 @@
+$docfxVersion = "2.24.0"
+$VisualStudioVersion = "15.0";
+$DotnetSDKVersion = "2.0.0";
+
+# Get dotnet paths
+$MSBuildExtensionsPath = "C:\Program Files\dotnet\sdk\" + $DotnetSDKVersion;
+$MSBuildSDKsPath = $MSBuildExtensionsPath + "\SDKs";
+
+# Get Visual Studio install path
+$VSINSTALLDIR =  $(Get-ItemProperty "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7").$VisualStudioVersion;
+
+# Add Visual Studio environment variables
+$env:VisualStudioVersion = $VisualStudioVersion;
+$env:VSINSTALLDIR = $VSINSTALLDIR;
+
+# Add dotnet environment variables
+$env:MSBuildExtensionsPath = $MSBuildExtensionsPath;
+$env:MSBuildSDKsPath = $MSBuildSDKsPath;
+
+# Build our docs
+& .\docfx.console.$docfxVersion\tools\docfx @args
