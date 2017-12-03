7e0bc60e0db6f3d834b5a41e1146b1ab561a424b
NZSmartie <nzsmartie@gmail.com>
2017-12-03 19:41:47 +1300

    Hold onto values and decode/encode them only when needed.

--- src/CoAPNet/Options/BlockWise.cs
+++ src/CoAPNet/Options/BlockWise.cs
@@ -21,67 +21,35 @@ namespace CoAPNet.Options
 
         public BlockBase(int optionNumber, int blockNumber, int blockSize, bool more) : this(optionNumber)
         {
-            SetStuff(blockNumber, SupportedBlockSizes.First(b => b.Item2 == blockSize).Item1, more);
+            BlockNumber = blockNumber;
+            BlockSize = blockSize;
+            IsMoreFollowing = more;
         }
 
         internal BlockBase(int optionNumber)
             :base (optionNumber, 0, 3, false, OptionType.UInt)
         { }
 
+        private int _blockSize = 1024;
+
         public int BlockSize
         {
-            get
-            {
-                var n = 0;
-                if (_length == 1)
-                    n = (int)(ValueUInt & 0xE0) >> 5;
-                if (_length == 2)
-                    n = (int)(ValueUInt & 0xE000) >> 13;
-                if (_length == 3)
-                    n = (int)(ValueUInt & 0xE00000) >> 21;
-
-                return SupportedBlockSizes.First(b => b.Item1 == n).Item2;
-            }
+            get => _blockSize;
             set
             {
-                SetStuff(BlockNumber, SupportedBlockSizes.First(b => b.Item2 == value).Item1, IsMoreFollowing);
+                if (!SupportedBlockSizes.Any(b => b.Item2 == value))
+                    throw new ArgumentOutOfRangeException();
+                _blockSize = value;
             }
         }
 
-        public bool IsMoreFollowing
-        {
-            get
-            {
-                if (_length == 0)
-                    return false;
-                if (_length == 1)
-                    return (ValueUInt & 0x10) != 0;
-                if (_length == 2)
-                    return (ValueUInt & 0x1000) != 0;
-                else if (_length == 3)
-                    return (ValueUInt & 0x100000) != 0;
-                throw new ArgumentOutOfRangeException();
-            }
-            set
-            {
-                SetStuff(BlockNumber, BlockSize, value);
-            }
-        }
+        public bool IsMoreFollowing { get; set; }
+
+        private int _blockNumber = 0;
 
         public int BlockNumber
         {
-            get
-            {
-                if (_length == 0)
-                    return 0;
-                if (_length == 1)
-                    return (int)(ValueUInt & 0x0F);
-                if (_length == 2)
-                    return (int)(ValueUInt & 0x0FFF);
-                else if (_length == 3)
-                    return (int)(ValueUInt & 0x0FFFFF);
-                throw new ArgumentOutOfRangeException();
-            }
+            get => _blockNumber;
             set
             {
                 if (value < 0)
@@ -89,30 +57,80 @@ namespace CoAPNet.Options
                 if (value > 0x0FFFFF)
                     throw new ArgumentOutOfRangeException("Can not be larger than 1,048,575 (2^20 - 1)");
 
-                SetStuff(value, BlockSize, IsMoreFollowing);
+                _blockNumber = value;
             }
         }
 
-        internal void SetStuff(int number, int size, bool more)
+        public override int Length
         {
-            var last = ((size & 0x07) << 5) | (more ? 0x10 : 0x00);
+            get
+            {
+                if (BlockNumber == 0 && BlockSize == 16 && IsMoreFollowing == false)
+                    return 0;
+                if (BlockNumber <= 0x0F)
+                    return 1;
+                if (BlockNumber <= 0x0FFF)
+                    return 2;
+                if (BlockNumber <= 0x0FFFFF)
+                    return 3;
+                throw new InvalidOperationException();
+            }
+        }
 
-            if (number <= 0x0F)
+        public override void FromBytes(byte[] data)
+        {
+            base.FromBytes(data);
+            uint last;
+
+            if (data.Length == 0)
             {
-                ValueUInt = (uint)(last | (number & 0x0F)) & 0xFF;
+                BlockNumber = 0;
+                last = 0;
             }
-            else if (number <= 0x0FFF)
+            else if (data.Length == 1)
             {
-                ValueUInt = (uint)((last << 8) | (number & 0x0FFF)) & 0xFFFF;
+                BlockNumber = (int)ValueUInt & 0x0F;
+                last = (ValueUInt & 0xF0) >> 4;
             }
-            else if (number <= 0x0FFFFF)
+            else if (data.Length == 2)
             {
-                ValueUInt = (uint)((last << 16) | (number & 0x0FFFFF)) & 0xFFFFFF;
+                BlockNumber = (int)ValueUInt & 0x0FFF;
+                last = (ValueUInt & 0xF000) >> 12;
+            }
+            else if (data.Length == 3)
+            {
+                BlockNumber = (int)ValueUInt & 0x0FFFFF;
+                last = (ValueUInt & 0xF00000) >> 20;
             }
             else
             {
-                throw new InvalidOperationException();
+                throw new CoapOptionException($"Invalid length ({data.Length}) of Block1/Block2 option");
             }
+
+            IsMoreFollowing = (last & 0x01) > 0;
+            var szx = (int)((last >> 1));
+            BlockSize = SupportedBlockSizes.First(b => b.Item1 == szx).Item2;
+        }
+
+        public override byte[] GetBytes()
+        {
+            var szx = SupportedBlockSizes.First(b => b.Item2 == BlockSize).Item1;
+            var last = (byte)((szx & 0x07) << 5) | (IsMoreFollowing ? 0x10 : 0x00);
+
+            if (BlockNumber <= 0x0F)
+            {
+                ValueUInt = (uint)(last | (BlockNumber & 0x0F));
+            }
+            else if (BlockNumber <= 0x0FFF)
+            {
+                ValueUInt = (uint)((last << 8) | (BlockNumber & 0x0FFF));
+            }
+            else if (BlockNumber <= 0x0FFFFF)
+            {
+                ValueUInt = (uint)((last << 16) | (BlockNumber & 0x0FFFFF));
+            }
+
+            return base.GetBytes();
         }
     }
 
