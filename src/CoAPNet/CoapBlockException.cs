a9e27bd5c5a188d3a0dedd4da26d54a5631468ec
NZSmartie <nzsmartie@gmail.com>
2017-12-04 15:41:22 +1300

    Add minimal documentation where it's missing

--- src/CoAPNet/CoapBlockException.cs
+++ src/CoAPNet/CoapBlockException.cs
@@ -5,24 +5,32 @@ using System.Text;
 
 namespace CoAPNet
 {
+    /// <summary>
+    /// Represents CoAP Block-Wise Transfer specific errors that occur during application execution.
+    /// </summary>
     [ExcludeFromCodeCoverage]
     public class CoapBlockException : CoapException
     {
+        /// <inheritdoc/>
         public CoapBlockException()
         { }
 
+        /// <inheritdoc/>
         public CoapBlockException(string message) 
             : base(message)
         { }
 
+        /// <inheritdoc/>
         public CoapBlockException(string message, CoapMessageCode responseCode) 
             : base(message, responseCode)
         { }
 
+        /// <inheritdoc/>
         public CoapBlockException(string message, Exception innerException) 
             : base(message, innerException)
         { }
 
+        /// <inheritdoc/>
         public CoapBlockException(string message, Exception innerException, CoapMessageCode responseCode) 
             : base(message, innerException, responseCode)
         { }
